51b60e70cc3942f53243cf6fc701bc997a8dfe6f
ILikeBananaPie <fishyniden@gmail.com>
2017-12-11 11:51:50 +0100

    GameObject added

---
+++ TheLittleWizard/TheLittleWizard/GameObject.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLittleWizard {
+    class GameObject {
+        public Vector2 position;
+
+        private GameObject() {
+            position = new Vector2();
+        }
+    }
+}
