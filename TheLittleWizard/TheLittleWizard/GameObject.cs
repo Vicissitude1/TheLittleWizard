12a55f6fccc7c2e862b7eb5ee349f944618a78d5
ILikeBananaPie <fishyniden@gmail.com>
2017-12-11 11:53:21 +0100

    a

--- TheLittleWizard/TheLittleWizard/GameObject.cs
+++ TheLittleWizard/TheLittleWizard/GameObject.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace TheLittleWizard {
-    class GameObject {
+    public class GameObject {
         public Vector2 position;
 
         private GameObject() {
