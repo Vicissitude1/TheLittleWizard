c407ee82e7df58db11432569ac5eb76866a8cb8a
ILikeBananaPie <fishyniden@gmail.com>
2017-12-11 14:16:20 +0100

    Synchro

--- TheLittleWizard/TheLittleWizard/Node.cs
+++ TheLittleWizard/TheLittleWizard/Node.cs
@@ -7,12 +7,20 @@ using System.Threading.Tasks;
 
 namespace TheLittleWizard {
     class Node : GameObject {
+        public Vector2 TilePos { get; private set; }
         public bool traversable;
         private GameObject another;
 
         public Node(int x, int y) {
+            TilePos = new Vector2(x, y);
             position = new Vector2(x * GameWorld.nodeSize, y * GameWorld.nodeSize);
             textureName = "groundSingleTile";
+            traversable = true;
+        }
+        public Node(int x, int y, string nameOfTexture) {
+            TilePos = new Vector2(x, y);
+            position = new Vector2(x * GameWorld.nodeSize, y * GameWorld.nodeSize);
+            textureName = nameOfTexture;
             traversable = true;
         }
 
