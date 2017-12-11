58954da54787abbdb36f0c5254a91cbf1c226aa1
ILikeBananaPie <fishyniden@gmail.com>
2017-12-11 11:48:39 +0100

    Prøver igen

--- TheLittleWizard/TheLittleWizard/Program.cs
+++ TheLittleWizard/TheLittleWizard/Program.cs
@@ -13,7 +13,7 @@ namespace TheLittleWizard {
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new GameWorld())
                 game.Run();
         }
     }
