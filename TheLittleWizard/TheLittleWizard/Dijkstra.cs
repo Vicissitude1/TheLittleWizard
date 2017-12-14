1075a3a8e0dcdf0b73ea8eb5b1ff195d46d20901
Daniel Madsen <d-madsen@live.dk>
2017-12-14 23:08:01 +0100

    j

---
+++ TheLittleWizard/TheLittleWizard/Dijkstra.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLittleWizard {
+    class Dijkstra {
+
+        public List<Node> fullCheck;
+        public List<Node> notFullCheck;
+
+
+        GameObject map;
+
+        private Node start;
+        private Node end;
+        private Node current;
+        bool allowDiagonal;
+
+        int iterations;
+
+        public int Iterations { get { return iterations; } }
+
+        public Dijkstra(GameObject map, Node start, Node end, bool allowDiagonal) {
+            this.map = map;
+            this.start = start;
+            this.end = end;
+            this.allowDiagonal = allowDiagonal;
+
+            fullCheck = new List<Node>();
+            notFullCheck = new List<Node>();
+            
+        }
+
+
+
+        
+    }
+}
